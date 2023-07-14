dcfb4b3af633934e161307f2b2b82c0a6e030c3f
kwngmin <kwngmink@gmail.com>
2023-07-14 17:54:44 +0900

    font 재설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,30 @@ import Header from "@/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
 import MainWrapper from "@/components/MainWrapper";
+import localFont from "next/font/local";
+
 // import { Inter } from "next/font/google";
 
 // const inter = Inter({ subsets: ["latin"] });
+const spoqaHanSans = localFont({
+  src: [
+    {
+      path: "./SpoqaHanSansNeo-Bold.woff2",
+      weight: "700",
+      style: "bold",
+    },
+    {
+      path: "./SpoqaHanSansNeo-Medium.woff2",
+      weight: "500",
+      style: "medium",
+    },
+    {
+      path: "./SpoqaHanSansNeo-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,8 +38,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="flex flex-col h-screen">
+    <html lang="ko">
+      <body className={`${spoqaHanSans.className} flex flex-col h-screen`}>
         <Header />
         <MainWrapper>{children}</MainWrapper>
       </body>
