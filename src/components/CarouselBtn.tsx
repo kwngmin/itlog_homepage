df07cecbdc8abdabbb0f5180a5fb0eefc1900baa
kwngmin <kwngmink@gmail.com>
2023-07-14 18:43:12 +0900

    캐로셀 네브 링 및 네브 배경 삭제

--- src/components/CarouselBtn.tsx
+++ src/components/CarouselBtn.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 export default function CarouselBtn({ btnName }: Props) {
   return (
-    <button className="flex-none w-fit md:ring-1 ring-slate-300 hover:ring-0 hover:bg-red-100 hover:text-red-500 font-medium px-6 rounded-full h-12">
+    <button className="flex-none w-fit hover:bg-red-100 hover:text-red-500 font-medium px-6 rounded-full h-12">
       {btnName}
     </button>
   );
