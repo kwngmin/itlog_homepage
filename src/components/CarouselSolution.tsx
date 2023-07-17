f67373a326a7bf508003b0d858d74e99292f7bda
kwngmin <kwngmink@gmail.com>
2023-07-17 10:10:08 +0900

    solution의 아이티로그 삭제

--- src/components/CarouselSolution.tsx
+++ src/components/CarouselSolution.tsx
@@ -8,11 +8,11 @@ export default function CarouselSolution() {
         Integrated
         <strong className="text-red-500"> IoT</strong> Solution
       </h1>
-      <p className="break-keep w-11/12 lg:w-9/12 tracking-tighter text-lg sm:text-xl md:text-2xl mt-2 leading-relaxed sm:leading-relaxed md:leading-relaxed text-black/70">
-        <strong className="font-bold text-red-500">아이티로그</strong>는 오랜
-        경력으로 쌓아온 노하우로 소프트웨어 및 하드웨어, 웹 서비스 그리고 높은
-        수준의 기술력과 전문성으로&nbsp;
-        <span className="text-black/90">
+      <p className="break-keep w-11/12 lg:w-8/12 tracking-tighter text-lg sm:text-xl md:text-2xl mt-2 leading-relaxed sm:leading-relaxed md:leading-relaxed text-black/70">
+        {/* <strong className="font-bold text-red-500">아이티로그</strong>는 오랜 */}
+        오랜 경력으로 쌓아온 노하우로 소프트웨어 및 하드웨어, 웹 서비스 그리고
+        높은 수준의 기술력과 전문성으로&nbsp;
+        <span className="text-black/90 font-medium">
           통합 <strong className="text-red-500">IoT</strong> 솔루션
         </span>
         을 제공합니다.
