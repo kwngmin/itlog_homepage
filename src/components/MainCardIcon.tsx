e7a4c74467b8ba356cccf3be3739e2b644356a40
kwngmin <kwngmink@gmail.com>
2023-07-18 12:17:44 +0900

    solution 텍스트 색상 변경

--- src/components/MainCardIcon.tsx
+++ src/components/MainCardIcon.tsx
@@ -32,7 +32,7 @@ export default function MainCardIcon({
     >
       <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white ">
         {cardNum}
-        <span className="text-red-700">{afterNum}</span>
+        <span className="text-red-800">{afterNum}</span>
       </h2>
       <p className="sm:text-lg md:text-xl lg:text-2xl tracking-tighter text-white/70 font-medium mb-2 md:mb-3">
         {cardText}
