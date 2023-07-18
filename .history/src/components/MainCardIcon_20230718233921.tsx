03fa7acf1e054bec607fcf4996b46d361d5cbdc7
kwngmin <kwngmin.k@gmail.com>
2023-07-18 23:43:30 +0900

    카드 노하우 텍스트 조정 및 삭제

--- .history/src/components/MainCardIcon_20230718233921.tsx
+++ .history/src/components/MainCardIcon_20230718233921.tsx
@@ -30,15 +30,14 @@ export default function MainCardIcon({
     <div
       className={`${colStart} overflow-hidden rounded-xl aspect-4/3 bg-red-500 flex-col justify-center items-center`}
     >
-      <h2 className="text-4xl lg:text-5xl font-bold text-white ">
+      <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white ">
         {cardNum}
         <span className="text-red-800 ml-0.5">{afterNum}</span>
       </h2>
       {/* <p className="sm:text-lg md:text-xl lg:text-2xl tracking-tighter text-white font-medium lg:font-normal mb-2 md:mb-3">
         {cardText}
       </p> */}
-      {/* <div className="sm:text-lg bg-red-600 rounded-full px-6 md:px-8 h-10 md:h-12 lg:h-14 md:pb-1 flex justify-center items-center md:text-xl lg:text-2xl tracking-tighter text-white "> */}
-      <div className="text-2xl lg:text-3xl tracking-tighter text-white ">
+      <div className="sm:text-lg bg-red-600 rounded-full px-6 md:px-8 h-10 md:h-12 lg:h-14 md:pb-1 flex justify-center items-center md:text-xl lg:text-2xl tracking-tighter text-white ">
         {cardSubText}
       </div>
     </div>
