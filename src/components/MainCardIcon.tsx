bd549ff3ee81ed556b8cda16a513a93cb3e241bb
kwngmin <kwngmink@gmail.com>
2023-07-18 14:32:55 +0900

    노하우 텍스트 테스트

--- src/components/MainCardIcon.tsx
+++ src/components/MainCardIcon.tsx
@@ -37,7 +37,7 @@ export default function MainCardIcon({
       {/* <p className="sm:text-lg md:text-xl lg:text-2xl tracking-tighter text-white font-medium lg:font-normal mb-2 md:mb-3">
         {cardText}
       </p> */}
-      <div className="sm:text-lg bg-red-700 rounded-full px-4 md:px-6 h-8 md:h-10 lg:h-12 sm:pb-0.5 flex justify-center items-center md:text-xl lg:text-2xl tracking-tighter text-white mt-1 ">
+      <div className="sm:text-lg bg-red-700 rounded-full px-4 md:px-6 h-10 md:h-12 lg:h-14 sm:mb-0.5 flex justify-center items-center md:text-xl lg:text-2xl tracking-tighter text-white mt-1 ">
         {cardSubText}
       </div>
     </div>
