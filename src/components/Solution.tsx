c2a74f9f6307abfa96a227973716bd88a641252d
kwngmin <kwngmink@gmail.com>
2023-07-19 10:13:49 +0900

    solution 텍스트 조정

--- src/components/Solution.tsx
+++ src/components/Solution.tsx
@@ -18,9 +18,10 @@ export default function Solution() {
         <strong className="text-red-500"> IoT</strong> Solution
       </h1>
       <p className="break-keep sm:w-10/12 md:w-11/12 tracking-tighter text-lg sm:text-xl md:text-2xl mt-2 leading-relaxed sm:leading-relaxed md:leading-relaxed text-black/70">
-        <strong className="font-bold text-red-500">아이티로그</strong>는 오랜
+        <strong className="font-bold text-red-500">아이티로그</strong>
+        {`는 오랜
         오랜 경력으로 쌓아온 노하우로 소프트웨어 및 하드웨어, 웹 서비스 그리고
-        높은 수준의 기술력과 전문성으로&nbsp;
+        높은 수준의 기술력과 전문성으로 `}
         <span className="text-black/90 font-medium">
           통합 <strong className="text-red-500">IoT</strong> 솔루션
         </span>
