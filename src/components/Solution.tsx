85c8b15bf550b578a0dc23d9c6ae3f7a17d047ea
kwngmin <kwngmink@gmail.com>
2023-07-19 10:04:35 +0900

    solution 텍스트 조정

--- src/components/Solution.tsx
+++ src/components/Solution.tsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Solution() {
   return (
     <div className="my-20 p-6 sm:px-12 py-16 rounded-2xl bg-gradient-to-br from-slate-100 to-red-50">
-      <h1 className="leading-tight break-keep text-2xl sm:text-4xl font-bold tracking-tight">
+      <h1 className="leading-tighter break-keep text-3xl sm:text-4xl font-bold tracking-tight">
         Integrated
         <strong className="text-red-500"> IoT</strong> Solution
       </h1>
-      <p className="break-keep sm:w-11/12 tracking-tighter text-xl sm:text-2xl mt-2 leading-relaxed sm:leading-relaxed text-black/70">
+      <p className="break-keep sm:w-11/12 tracking-tighter text-lg sm:text-xl md:text-2xl mt-2 leading-relaxed sm:leading-relaxed md:leading-relaxed text-black/70">
         <strong className="font-bold text-red-500">아이티로그</strong>는 오랜
         오랜 경력으로 쌓아온 노하우로 소프트웨어 및 하드웨어, 웹 서비스 그리고
         높은 수준의 기술력과 전문성으로&nbsp;
