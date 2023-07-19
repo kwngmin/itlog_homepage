2cbe2c362321dfdd26738c313a4e877866ee5bd8
kwngmin <kwngmink@gmail.com>
2023-07-19 11:09:56 +0900

    opengraph 메인

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,33 @@ import type { Metadata } from "next";
 import MainWrapper from "@/components/MainWrapper";
 import Head from "next/head";
 import Footer from "@/components/Footer";
+// import favicon from "../../public/favicon.ico";
 
 export const metadata: Metadata = {
-  title: "ITLOG",
+  title: "itlog - (주)아이티로그",
   description:
-    "건설안전 IT기업/건설 CCTV 전문/타워크레인 카메라/풍속계/네트워크공사/방송시스템",
+    "(주)아이티로그 - 건설은안전하게,안전은스마트하게,건설안전관리의파트너",
+  openGraph: {
+    title: "Next.js",
+    description: "The React Framework for the Web",
+    url: "https://nextjs.org",
+    siteName: "itlog - (주)아이티로그",
+    images: [
+      {
+        url: "../../public/images/opengraph_800.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "../../public/images/opengraph_1600.png",
+        width: 1800,
+        height: 1600,
+        alt: "itlog logo",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -28,6 +50,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
         <meta name="format-detection" content="telephone=no" />
+        {/* <link rel="shortcut icon" href="../../public/favicon.ico" /> */}
       </head>
       <body className={`flex flex-col h-screen relative`}>
         <Header />
