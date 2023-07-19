70a2af9ffb6b697683576fbfe76d9f2e367ad95e
kwngmin <kwngmink@gmail.com>
2023-07-19 09:49:36 +0900

    solution 캐로셀 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import NewBroadCast from "@/components/NewBroadCast";
 import NewSafety from "@/components/NewSafety";
 import Instance from "@/components/Instance";
 import HelpDesk from "@/components/HelpDesk";
+import Solution from "@/components/Solution";
 
 export default function Home() {
   return (
@@ -21,12 +22,13 @@ export default function Home() {
       <MainFull />
       <BodyWrapper>
         <CardKnowHow />
-        <CarouselSolution />
+        <Solution />
         <NewGateSystem />
         <NewTowerSystem />
         <NewSensors />
         <NewSafety />
         <NewBroadCast />
+        <CarouselSolution />
         <Instance />
         <HelpDesk />
       </BodyWrapper>
---
+++ src/components/Solution.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Solution() {
+  return (
+    <div className="my-20 p-8 md:px-12 py-16 rounded-2xl bg-gradient-to-br from-slate-100 to-red-50 ">
+      <h1 className="leading-tight break-keep text-3xl sm:text-4xl font-bold tracking-tight">
+        Integrated
+        <strong className="text-red-500"> IoT</strong> Solution
+      </h1>
+      <p className="break-keep md:w-11/12 tracking-tighter text-xl md:text-2xl mt-2 leading-relaxed md:leading-relaxed md:leading-relaxed text-black/70">
+        <strong className="font-bold text-red-500">아이티로그</strong>는 오랜
+        오랜 경력으로 쌓아온 노하우로 소프트웨어 및 하드웨어, 웹 서비스 그리고
+        높은 수준의 기술력과 전문성으로&nbsp;
+        <span className="text-black/90 font-medium">
+          통합 <strong className="text-red-500">IoT</strong> 솔루션
+        </span>
+        을 제공합니다.
+      </p>
+    </div>
+  );
+}
